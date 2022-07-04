bc294b228bc4bdb067ec38b6526782b10a4a62a9
Luc1d1ty <hasanmahbub498@gmail.com>
2022-07-04 14:09:43 +0600

    Fixed error / flase logic . added do-while loop to check numbers until I want to stop :D

--- CPP/Number_Theory/Prime_Numbers/check_prime_bf.cpp
+++ CPP/Number_Theory/Prime_Numbers/check_prime_bf.cpp
@@ -1,28 +1,34 @@
 #include <bits/stdc++.h>
 
-#define fastio                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL)
-
 #define endl "\n"
 
 using namespace std;
 
 int main(void)
 {
-    fastio;   // fast input output
-
-    int num;
-    cin >> num ;
-
-    for(int i = 2 ; i < num ; i++ ){ // i = 2 because we are checking for prime numbers from 2 to num-1
-        if(num % i == 0 ){ // if num is divisible by i then it is not prime
-            cout << "False"; 
-            break; 
+    int num = -1;
+    do
+    {
+        bool flag = true;
+        cout << "\n Enter a number . to end the program enter / press 0 : ";
+        cin >> num;
+        if (num <= 1)
+        {
+            flag = false;
         }
-        else { // if num is not divisible by i then it is prime
-            cout << "True"; 
+        else
+        {
+            for (int i = 2; i < num; i++)
+            { // i = 2 because we are checking for prime numbers from 2 to num-1
+                if (num % i == 0)
+                { // if num is divisible by i then it is not prime
+                    flag = false;
+                    break;
+                }
+            }
         }
-    }
-    
+
+        flag == true ? cout << "The number " << num << " is prime " << endl : cout << "Unfortunately , The number " << num << " is not prime " << endl;
+
+    } while (num != 0);
 }
