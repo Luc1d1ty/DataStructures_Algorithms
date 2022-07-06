df195e271ceeba6daac101c39fe90c3d0c0516e3
Luc1d1ty <hasanmahbub498@gmail.com>
2022-07-06 13:01:58 +0600

    Added modular exponentiation

---
+++ CPP/Number_Theory/Modular_exponentiation/mod_ex_optimized.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define endl "\n"
+#define ll long long
+
+using namespace std;
+
+ll modEx(int x, int n, int m)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    x = x % m;
+
+    ll result = 1;
+
+    while (n > 0)
+    {
+        if (n % 2)
+        {
+            result = (result * x) % m;
+        }
+        x = (x * x) % m;
+        n /= 2;
+    }
+
+    return result;
+}
+
+int main()
+{
+    fastio;
+
+    int x, n, m;
+
+    cin >> x >> n >> m;
+
+    cout << modEx(x, n, m) << endl;
+    return 0;
+}
---
+++ CPP/Number_Theory/Modular_exponentiation/simple_mod_ex.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+int modEx(int x, int n, int m)
+{
+
+    if (n == 0)
+    {
+        return 1;
+    }
+    else if (n % 2 == 0)
+    {
+        int powr = pow(x, n / 2);
+        return ((powr % m) * (powr % m)) % m;
+    }
+    else
+    {
+        int powr = pow(x, n - 1);
+        return ((x % m) * (powr % m)) % m;
+    }
+}
+
+int main()
+{
+    fastio;
+    int x, y, m;
+    cin >> x >> y >> m;
+    cout << modEx(x, y, m) << endl;
+
+    return 0;
+}
