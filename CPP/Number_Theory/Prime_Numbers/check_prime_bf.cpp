6d80abb514cc4ebfb74f94839716cd52e3d0df8b
Mahbub <hasanmahbub498@gmail.com>
2023-07-01 19:30:23 +0600

    updates to check_prime_bf

--- CPP/Number_Theory/Prime_Numbers/check_prime_bf.cpp
+++ CPP/Number_Theory/Prime_Numbers/check_prime_bf.cpp
@@ -4,31 +4,35 @@
 
 using namespace std;
 
+bool checkPrime(int num)
+{
+    if (num <= 1)
+    {
+        cout << "The number "<< num << " is not prime because it is less than or equal to 1 . Program will end now ." << endl;
+        exit(0);
+    }
+    else
+    {
+        for (int i = 2; i <= sqrt(num); i++)
+        { // i = 2 because we are checking for prime numbers from 2 to square root of num 
+            if (num % i == 0)
+            { // if num is divisible by i then it is not prime
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 int main(void)
 {
     int num = -1;
     do
     {
-        bool flag = true;
-        cout << "\n Enter a number . to end the program enter / press 0 : ";
+        cout << "\n Enter a number to check prime . to end the program enter a number less than or equal 1 : ";
         cin >> num;
-        if (num <= 1)
-        {
-            flag = false;
-        }
-        else
-        {
-            for (int i = 2; i < num; i++)
-            { // i = 2 because we are checking for prime numbers from 2 to num-1
-                if (num % i == 0)
-                { // if num is divisible by i then it is not prime
-                    flag = false;
-                    break;
-                }
-            }
-        }
+        
+        checkPrime(num) ? cout << "\nThe number " << num << " is prime " << endl : cout << "\nUnfortunately , The number " << num << " is not prime " << endl;
 
-        flag == true ? cout << "The number " << num << " is prime " << endl : cout << "Unfortunately , The number " << num << " is not prime " << endl;
-
-    } while (num != 0);
+    } while (num > 1);
 }
